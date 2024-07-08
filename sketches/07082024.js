473f0e5fb9e132607fffa70ae1e310b728b23f4a
John Eckert <johnteckert@gmail.com>
2024-07-08 15:12:50 -0400

    07082024

---
+++ sketches/07082024.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  noFill();
+}
+
+function draw() {
+  for (let l = 0; l < windowWidth; l += 1000 * Math.random()) {
+    let lineColor = color('#ffd452');
+    push();
+    strokeWeight(0.5);
+    stroke(lineColor);
+    translate(noise(), 0, 0);
+    line(l, 0, l, windowWidth);
+    pop();
+  }
+
+  for (let i = 0; i < windowWidth + 500; i+= 800 * Math.random()) {
+    let circleColor = lerpColor(color(173, 83, 137), color(60, 16, 83), i / windowWidth);
+    push();
+    strokeWeight(1);
+    stroke(circleColor);
+    circle(windowWidth / 2, windowHeight / 2, i);
+    pop();
+  }
+}
