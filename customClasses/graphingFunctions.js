2ea41943d083fff1534d40246bbba2887489e5ff
John Eckert <johnteckert@gmail.com>
2024-07-24 17:59:40 -0400

    add 3 color gradient

--- customClasses/graphingFunctions.js
+++ customClasses/graphingFunctions.js
@@ -27,6 +27,21 @@ function backgroundGradient(c1, c2) {
   }
 }
 
+function backgroundGradientThreeColor(c1, c2, c3) {
+  for(let y=0; y<height; y++){
+    if (y < height / 2) {
+      n = map(y, 0 ,height / 2, 0, 1);
+      let newc = lerpColor(c1,c2,n);
+      stroke(newc);
+    } else {
+      n = map(y, height / 2, height, 0, 1);
+      let newc = lerpColor(c2,c3,n);
+      stroke(newc);
+    }
+    line(0,y,width, y);
+  }
+}
+
 function backgroundDisco(xSize, ySize, c1, c2) {
   push();
   for(let y=0; y<height; y += ySize){
@@ -40,6 +55,40 @@ function backgroundDisco(xSize, ySize, c1, c2) {
       fill(fillColor);
       square(x,y,xSize);
     }
+  }
+  pop();
+}
+
+function trianglePattern(xSize, ySize, c1, c2) {
+  push();
+  translate(-xSize / 2, -ySize / 2);
+  let col = 0;
+  let row = 0;
+  for(let y=0; y<height; y += ySize){
+    for(let x=0; x<width * 2; x += xSize / 2){
+      n = randomGaussian(0.5, 0.5);
+      let strokeColor = lerpColor(c1,c2,0.8);
+      let fillColor = lerpColor(c1,c2,n);
+      stroke(strokeColor);
+      strokeWeight(1);
+      // noStroke();
+      fill(fillColor);
+      if (row % 2 == 0) {
+        if (col % 2 == 0) {
+          triangle(x, y, x + xSize, y, x + xSize/2, y + ySize);
+        } else {
+          triangle(x + xSize / 2, y, x, y + ySize, x + xSize, y + ySize);
+        }
+      } else {
+        if (col % 2 == 0) {
+          triangle(x + xSize / 2, y, x, y + ySize, x + xSize, y + ySize);
+        } else {
+          triangle(x, y, x + xSize, y, x + xSize/2, y + ySize);
+        }
+      }
+      row += 1;
+    }
+    col += 1;
   }
   pop();
 }
