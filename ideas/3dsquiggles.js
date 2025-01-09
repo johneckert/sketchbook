f0dc01a656536a61b01feba78468408961bc3593
John Eckert <johnteckert@gmail.com>
2025-01-08 22:52:28 -0500

    ideas

--- ideas/3dsquiggles.js
+++ ideas/3dsquiggles.js
@@ -4,7 +4,6 @@
 
 const ONE_MILLION = 1000000;
 let ball, ring, cam;
-let balls = [];
 
 function setup() {
   angleMode(DEGREES);
@@ -12,25 +11,19 @@ function setup() {
   //Default Camera -  x: 0, y: 0, z: 800, centerX: 0, centerY: 0, centerZ: 0, upX: 0, upY: 0, upZ: 1.
   cam = createCamera();
   setCamera(cam);
-
-  // ball = new Ball(800);
+  rotate(90, [0, 0, 1]);
   ring = new Ring(500, 20);
-  balls.push(new Ball(50));
+  // ring.display();
+  
 }
 
 function draw() {
-  background(255);
-  pointLight(255, 0, 255, 0, 0, 0);
-
-  noFill();
-  // ball.display();
-  balls.forEach(ball => {
-    ball.display();
-  });
-
-  push();
+  noFill(); 
+  strokeWeight(0.3);
+  stroke(random(255), random(255), random(255));
+  translate(frameCount* random(-25, 25), frameCount* random(-25, 25), 0);
+  rotate(frameCount* random(0.5, 5), [1, 1, 1]);
   ring.display();
-  pop();
 }
 
 class Ring {
@@ -46,9 +39,6 @@ class Ring {
 
   display() {
     for (let face of this.faces) {
-      strokeWeight(0.1);
-      stroke(0);
-
       // Draw the face.
       beginShape();
       // Iterate over the vertices that form the face.
@@ -84,8 +74,8 @@ class Ball {
       for (let f of face) {
         // Get the vertex's p5.Vector object.
         let v = this.geometry.vertices[f];
-        vertex(v.x, v.y, v.z);
-        vertex(v.x, v.y, v.z * sin(frameCount) * 7);
+        translate(v);
+        sphere(10)
       }
       endShape();
     }
