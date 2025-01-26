95ef1ba556ec41216bb965f450561b3e3819aea0
John Eckert <johnteckert@gmail.com>
2025-01-26 17:30:47 -0500

    01262025

--- sketches/01262025.js
+++ sketches/01262025.js
@@ -2,34 +2,24 @@
 // https://genuary.art/
 // Prompt: Symmetry.
 
-let aDir, bDir, a, b, maxA, maxB, centerX, centerY;
-let vertices = [];
-
-
+let a, b;
 
 function setup() {
   noFill();
   createCanvas(windowWidth, windowHeight);
 
-  aDir = 1;
-  bDir = 1;
-  centerX = 0; 
-  centerY = 0;
-  a = 3;
-  b = 3;
-  maxA = width / 2;
-  maxB = height / 2;
+  a = width / 10 * 4;
+  b = height / 10;
   backgroundGradient(color('#3F5EFB'), color('#FC466B'));
 }
 
 function draw() {
-  // backgroundGradient(color('#3F5EFB'), color('#FC466B'));
   translate(width / 2, height / 2);
-  strokeWeight(0.5);
-  stroke(lerpColor(color('#bfe9ff'), color('#ff6e7f'), map(sin(frameCount * 0.01), -1, 1, 0, 1)));
+  strokeWeight(0.1);
+  stroke(lerpColor(color('#3F5EFB'), color('#FC466B'), map(sin(frameCount * 0.01), -1, 1, 0, 1)));
   
   push();
-  rotate(0);
+  rotate(random(-0.1, 0.1));
   beginShape();
   for (let t = 0; t < TWO_PI; t += 0.01) {
     let x = a * sin(t);
@@ -48,30 +38,22 @@ function draw() {
   }
   endShape(CLOSE);
   pop();
-
-  a += 1.5 * aDir;
-  b += 0.5 * bDir;
-  if (a >= maxA || a <= 3) {
-    if (a <= 3) {
-      maxA += 5
-    }
-    aDir *= -1;
-  }
-  if (b >= maxB || b <= 3) {
-    if (b <= 3) {
-      maxB += 5
-    }
-    bDir *= -1;
-  }
 }
 
 function backgroundGradient(c1, c2) {
   push();
-  for(let y=0; y<height; y++){
-    n = map(y,0,height,0,1);
-    let newc = lerpColor(c1,c2,n);
+  for(let y = 0; y < height / 2; y++){
+    n = map(y, 0, height, 0, 1);
+    let newc = lerpColor(c1, c2, n);
     stroke(newc);
-    line(0,y,width, y);
+    line(0, y, width, y);
+  }
+
+  for(let y = height / 2; y < height; y++){
+    n = map(y, 0, height, 1, 0);
+    let newc = lerpColor(c1, c2, n);
+    stroke(newc);
+    line(0, y, width, y);
   }
   pop();
 }
